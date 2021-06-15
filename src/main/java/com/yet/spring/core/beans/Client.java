5525a58c1471e1aeafacc29955fc45863f0ee5b1
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-15 22:17:01 +0300

    Add Client entity

---
+++ src/main/java/com/yet/spring/core/beans/Client.java
@@ -0,0 +1,52 @@
+package com.yet.spring.core.beans;
+
+import java.util.Objects;
+
+public class Client {
+    private int id;
+    private String fullName;
+
+    public Client() {}
+
+    public Client(int id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        var client = (Client) o;
+        return id == client.id && Objects.equals(fullName, client.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fullName);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                '}';
+    }
+}
