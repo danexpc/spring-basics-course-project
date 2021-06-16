d0ecefc395f7c12de94af6624374cd556d72623a
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-16 10:36:31 +0300

    Add Date Formatter to Event class

--- src/main/java/com/yet/spring/core/events/Event.java
+++ src/main/java/com/yet/spring/core/events/Event.java
@@ -1,5 +1,6 @@
 package com.yet.spring.core.events;
 
+import java.text.DateFormat;
 import java.util.Date;
 import java.util.Random;
 
@@ -7,9 +8,11 @@ public class Event {
     private final int id = new Random().nextInt();
     private String msg;
     private final Date date;
+    private final DateFormat df;
 
-    public Event(Date date) {
+    public Event(Date date, DateFormat df) {
         this.date = date;
+        this.df = df;
     }
 
     public int getId() {
@@ -33,7 +36,7 @@ public class Event {
         return "Event{" +
                 "id=" + id +
                 ", msg='" + msg + '\'' +
-                ", date=" + date +
+                ", date=" + df.format(date) +
                 '}';
     }
 }
