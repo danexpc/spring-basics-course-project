c9e9fd249d1ec3bbeba9a7f9a5171bfcbd8e4a21
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-16 15:05:09 +0300

    Provide a list of loggers as constructor argument in xml config file

--- src/main/java/com/yet/spring/core/App.java
+++ src/main/java/com/yet/spring/core/App.java
@@ -2,18 +2,22 @@ package com.yet.spring.core;
 
 import com.yet.spring.core.beans.Client;
 import com.yet.spring.core.beans.Event;
+import com.yet.spring.core.beans.EventType;
 import com.yet.spring.core.loggers.EventLogger;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Map;
+
 public class App {
     private final Client client;
-    private final EventLogger eventLogger;
+    private final EventLogger defaultLogger;
+    private final Map<EventType, EventLogger> loggers;
 
-    public App (Client client, EventLogger eventLogger) {
+    public App (Client client, EventLogger eventLogger, Map<EventType, EventLogger> loggers) {
         this.client = client;
-        this.eventLogger = eventLogger;
+        this.defaultLogger = eventLogger;
+        this.loggers = loggers;
     }
 
     public static void main(String[] args) {
@@ -32,6 +36,5 @@ public class App {
     public void logEvent(Event event, String msg) {
         var message = msg.replaceAll(String.valueOf(client.getId()), client.getFullName());
         event.setMsg(message);
-        eventLogger.logEvent(event);
     }
 }
