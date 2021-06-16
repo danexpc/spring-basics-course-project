bb07622e6b49fa5cd0805c7af2e442d983a5dbb2
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-16 09:10:32 +0300

    Designate ConsoleEventLogger as the class that implements EventLogger

--- src/main/java/com/yet/spring/core/loggers/ConsoleEventLogger.java
+++ src/main/java/com/yet/spring/core/loggers/ConsoleEventLogger.java
@@ -1,6 +1,6 @@
 package com.yet.spring.core.loggers;
 
-public class ConsoleEventLogger {
+public class ConsoleEventLogger implements EventLogger {
     public void logEvent(String msg) {
         System.out.println(msg);
     }
