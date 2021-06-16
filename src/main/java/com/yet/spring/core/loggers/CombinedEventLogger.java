4cf4b34ee5755daf8a935b8a66a540373aec864c
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-16 14:51:57 +0300

    Create combined event logger

---
+++ src/main/java/com/yet/spring/core/loggers/CombinedEventLogger.java
@@ -0,0 +1,19 @@
+package com.yet.spring.core.loggers;
+
+import com.yet.spring.core.beans.Event;
+
+import java.util.Collection;
+
+public class CombinedEventLogger implements EventLogger {
+    private final Collection<EventLogger> loggers;
+
+    public CombinedEventLogger(Collection<EventLogger> loggers) {
+        super();
+        this.loggers = loggers;
+    }
+
+    @Override
+    public void logEvent(Event event) {
+        loggers.forEach(logger -> logger.logEvent(event));
+    }
+}
